ef427787db4d524023a11462447015ef03ba5029
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2022-09-05 21:49:15 +0100

    adding schema variable

--- api/users/user.model.js
+++ api/users/user.model.js
@@ -1,11 +1,25 @@
-var mongoose = require('mongoose');
+// Fichero que nos dice mongoose que nos tenemos que crear
+var mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    name: String,
-    id: Number
+  user_Status: Boolean,
+  city: String,
+  datebirth: String,
+  direction: String,
+  dni: String,
+  email: String,
+  full_info: String,
+  id: String,
+  img: String,
+  job: String,
+  name: String,
+  pass: String,
+  price: String,
+  schedule: String,
+  score: Number,
+  worker: Boolean,
 });
 
-const UserModel = mongoose.model('user', UserSchema);
-
+const UserModel = mongoose.model("user", UserSchema);
 
 module.exports = UserModel;
