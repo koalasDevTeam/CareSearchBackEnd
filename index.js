600fe280447afc3d4ef60f17421d4dc5419515d7
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2022-09-05 21:51:59 +0100

    adding to detect errors, and cors

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors"); //specify which origins can access the API/backend
+app.use(cors({ origin: "*" })); //"http://localhost:4200" * ignifica que todos tiene acceso a llamar al backend
+/*  */
+const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 
-// var mongoose = require('mongoose')
-// mongoose.connect(`mongodb://localhost/care-search-db`)
+//to connect thought mongodb Atlas
+
+/* mongoose.connect(
+  `mongodb+srv://dbUser:${MONGO_DB_PASSWORD}@cluster0.zjngkqi.mongodb.net/Koala-project?retryWrites=true&w=majority`
+); */
+
+//to connect through localhost 27017
+mongoose.connect(`mongodb://localhost/Koala-project`);
+
+// to detect errors
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.use(express.json());
 
