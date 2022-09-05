cd7247961683055a3df7d1f0e0a718206fa11cc2
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2022-09-05 21:59:17 +0100

    adding Readme file

---
+++ Readme.md
@@ -0,0 +1,12 @@
+this is how you run the Back-end
+
+
+PowerShell
+```ps
+$env:MONGO_DB_PASSWORD="<PASSOWRD>"; nodemon
+```
+
+Bash (no tested)
+```bash
+MONGO_DB_PASSWORD="<PASSOWRD>" nodemon
+```
--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,10 +1,12 @@
-const router = require('express').Router()
-const controller = require('./users.controller');
+// Este fichero se encarga de definir las urls, los verbo. Este fichero no va a crear ni borrar cosas, se encargara de traer de otro fichero(controller) todas las funciones
 
-router.get('/', controller.getAll)
-router.get('/:id', controller.getOneById)
-router.post('/', controller.create)
-router.delete('/:id', controller.removeOneById)
-router.put('/:id', controller.updateOneById)
+const router = require("express").Router();
+const controller = require("./users.controller");
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getOneById);
+router.post("/", controller.create);
+router.delete("/:id", controller.removeOneById);
+router.put("/:id", controller.updateOneById);
 
 module.exports = router;
