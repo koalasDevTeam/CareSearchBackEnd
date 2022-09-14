a5932cc4635aabd1ad69648629bbc389eca0b52d
Sabrina-MM <sabrinamarisolmartinez@gmail.com>
2022-09-14 21:09:33 +0100

    adding hasheado para password in updatebyId

--- api/users/users.controller.js
+++ api/users/users.controller.js
@@ -22,7 +22,6 @@ function removeSensitiveData(arrayOfUsers) {
 }
 
 function getAll(req, res) {
-  console.log(req.user);
   return UserModel.find({}) //find all users, puedes poner parametro o no.
     .then((users) => {
       users = users.filter((user) => user.worker === true); // bringing just  workers
@@ -51,7 +50,7 @@ function islogin(req, res) {
   return UserModel.findOne({ email: req.body.email }).then(async (user) => {
     if (!user) {
       return res
-        .status(400)
+        .status(401)
         .json({ message: "Usuario o contrasen~a sincorrectos" }); // wrong email
     }
     try {
@@ -84,8 +83,6 @@ async function register(req, res) {
   try {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.pass, salt);
-    console.log(salt);
-    console.log(hashedPassword);
     req.body.pass = hashedPassword;
     return UserModel.create(req.body) //you give UserModel the body you want
       .then((userCreated) => {
@@ -117,19 +114,68 @@ function updateOneById(req, res) {
     res.status(500).send({ error_message: " datebirth: is missing" });
     return;
   } */
-  console.log(req.user._id);
-  return UserModel.findByIdAndUpdate(req.user._id, req.body, {
-    new: true,
-    runValidators: true, // validate each line, and do not do whatever the user wants
-  })
-    .then((updated) => {
-      updated = removeUserPassword([updated])[0];
+  //console.log(req.body.pass, req.body.oldPass);
+  if (req.body.pass && req.body.oldPass) {
+    // entra aqui si la peticion viene del formulario de actualizar la password
 
-      return res.send(updated);
+    if (req.body.pass !== req.body.oldPass) {
+      UserModel.findOne({ email: req.body.email }).then(async (user) => {
+        if (!user) {
+          return res
+            .status(401)
+            .json({ message: "Usuario o contrasen~a sincorrectos" }); // wrong email
+        }
+        try {
+          // si la contrase;a que envia el usuario como su contra;a actual es igual que la que tiene en la db...
+          // entonces actualiza su contrase;a
+          if (await bcrypt.compare(req.body.oldPass, user.pass)) {
+            // TODO: si la contrase;a que esta en db es la misma que se que se quiere cambiar etnonces devuelve error.
+            const salt = await bcrypt.genSalt();
+            const hashedPassword = await bcrypt.hash(req.body.pass, salt);
+            req.body.pass = hashedPassword;
+            return UserModel.findByIdAndUpdate(req.user._id, req.body, {
+              new: true,
+              runValidators: true, // validate each line, and do not do whatever the user wants
+            })
+              .then((updated) => {
+                updated = removeUserPassword([updated])[0];
+
+                return res.send(updated);
+              })
+              .catch((err) => {
+                res.status(500).send(err);
+              });
+          } else {
+            return res
+              .status(401)
+              .send({ message: "Usuario o contrasen~a son incorrectos" }); //wrong password
+          }
+        } catch (err) {
+          return res
+            .status(401)
+            .send({ message: "Usuario o contrasen~a son incorrectos" });
+        }
+      });
+    } else {
+      return res
+        .status(401)
+        .send({ message: "Misma contrase'a la antigua y la nueva" });
+    }
+  } else {
+    // entra aqui si la peticion viene del formulario de actualizar el perfil
+    return UserModel.findByIdAndUpdate(req.user._id, req.body, {
+      new: true,
+      runValidators: true, // validate each line, and do not do whatever the user wants
     })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+      .then((updated) => {
+        updated = removeUserPassword([updated])[0];
+
+        return res.send(updated);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  }
 }
 
 // Deleting a user
