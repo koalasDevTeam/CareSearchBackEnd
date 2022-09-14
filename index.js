014481ec754408a79436ef18ec148b193b38703a
Z3B3N-dev <zebensui.profesional84@gmail.com>
2022-09-14 15:35:45 +0100

    esta ok

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(cors({ origin: "*" })); //"http://localhost:4200" * ignifica que todos t
 /*  */
 const MONGO_DB_PASSWORD = process.env.MONGO_DB_PASSWORD;
 if (!process.env.ACCESS_TOKEN_SECRET) {
-  console.log("ACCESS_TOKEN_SECRET is missing");
-  /* res.status(500).send({ message: "ACCESS_TOKEN_SECRET is missing" }); */
-  process.exit(1);
+    console.log("ACCESS_TOKEN_SECRET is missing");
+    /* res.status(500).send({ message: "ACCESS_TOKEN_SECRET is missing" }); */
+    process.exit(1);
 }
 
 //to connect thought mongodb Atlas
@@ -23,8 +23,8 @@ mongoose.connect(`mongodb://localhost/Koala-project`);
 // to detect errors
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.once("open", function() {
+    console.log("Connected successfully");
 });
 
 app.use(express.json()); // it must be here always, on top of all funsctions to be able to read json, otherwise you will not be able to see the post
